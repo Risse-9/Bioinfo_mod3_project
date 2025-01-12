72ba03951f8cc1cd749e10e5a095f5fbcde74868
Risse-9 <nenemarionel1@gmail.com>
2025-01-12 17:21:46 +0100

    modified python script

--- Data/blast_automate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script to perform BLAST+ workflow
-
-makeblastdb -in nucleotide_fasta_protein_homolog_model.fasta -dbtype nucl -out CARD_DB
-
-for genome in GCA*.fna; do
-  output="${genome%.fna}_blast_results.txt"
-  blastn -query "$genome" -db CARD_DB -out "$output" -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore" -evalue 1e-30
-done
